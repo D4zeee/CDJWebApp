b9d9de00b5516b1a8a09f301f24d7385fd8dce3d
Joshua Daniel Pusing <joshuadanielpusing@gmail.com>
2025-09-22 01:46:45 +0800

    Spring boot setup

---
+++ backend/cdj-backend/cdj-backend/src/main/java/com/cdj_backend/CdjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdj_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdjBackendApplication.class, args);
+	}
+
+}
