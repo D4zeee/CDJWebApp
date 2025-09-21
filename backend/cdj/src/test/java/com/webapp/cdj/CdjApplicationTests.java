5386d2f39a04a356e7bcd9852e5787846fab388e
Joshua Daniel Pusing <joshuadanielpusing@gmail.com>
2025-09-22 01:55:52 +0800

    Spring boot setup

---
+++ backend/cdj/src/test/java/com/webapp/cdj/CdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.cdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
